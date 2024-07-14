6f12c39fd272f1bc87dd2a7afa109715dc3efe55
Robelio Barreto <robeliobasantos@gmail.com>
2024-07-14 15:14:26 -0300

    Utilizando o RestTemplate e o ObjectMapper

--- src/main/java/br/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/br/com/alura/literalura/LiteraluraApplication.java
@@ -1,13 +1,26 @@
 package br.com.alura.literalura;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class LiteraluraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
 	}
 
 }
